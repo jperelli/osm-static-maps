c764489baa08589d46fb89dc04adb2f4094f7887
Julian Perelli <jperelli@gmail.com>
2020-01-12 22:52:38 +0100

    allow POST data in sample server

--- sample-server.js
+++ sample-server.js
@@ -3,42 +3,36 @@ var express = require("express"),
   osmsm = require("./lib/lib.js");
 
 var app = express();
-
-// all environments
 app.set("port", process.env.PORT || 3000);
 app.set("views", __dirname + "/lib");
 app.set("view engine", "handlebars");
 app.set("view options", { layout: false });
+app.use(express.json());
 
-http.createServer(app).listen(app.get("port"), function() {
-  console.log("Express server listening on port " + app.get("port"));
-});
+http
+  .createServer(app)
+  .listen(app.get("port"), () =>
+    console.log("Express server listening on port " + app.get("port"))
+  );
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   console.log("[" + new Date().toISOString() + "] " + req.originalUrl);
   next();
 });
 
-app.get("/", function(req, res) {
-  osmsm(req.query)
-    .catch(function(err) {
-      res.status(500)
-      res.end(err.toString());
-    })
-    .then(function(imageBinary) {
-      res.end(imageBinary, "binary");
-    });
-});
+const handler = (res, params) => {
+  osmsm(params)
+    .then(data => res.end(data))
+    .catch(err => res.status(500).end(err.toString()));
+};
 
-app.get("/dynamic", function(req, res) {
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  req.query.renderToHtml = true;
-  osmsm(req.query)
-    .catch(function(err) {
-      res.status(500);
-      res.end(err.toString());
-    })
-    .then(function(html) {
-      res.end(html);
-    });
-});
+app.get("/", (req, res) => handler(res, req.query));
+app.post("/", (req, res) => handler(res, req.body));
+
+app.get("/dynamic", (req, res) =>
+  handler(res, { ...req.query, renderToHtml: true })
+);
+
+app.post("/dynamic", (req, res) =>
+  handler(res, { ...req.body, renderToHtml: true })
+);
