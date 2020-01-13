29d82054cc52ad3bd36a448306ddc4630d2dcf4b
Julian Perelli <jperelli@gmail.com>
2020-01-13 01:40:04 +0100

    make the post limit bigger

--- sample-server.js
+++ sample-server.js
@@ -7,7 +7,7 @@ app.set("port", process.env.PORT || 3000);
 app.set("views", __dirname + "/lib");
 app.set("view engine", "handlebars");
 app.set("view options", { layout: false });
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 http
   .createServer(app)
